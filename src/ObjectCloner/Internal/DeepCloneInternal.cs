448f2e9aa14adaf34cc9eb2e80399c7b261b3fde
Marcell Toth <marcell@marcelltoth.net>
2019-07-29 00:16:10 +0200

    DeepClone WIP
    Performs basic shallow copies.

--- src/ObjectCloner/Internal/DeepCloneInternal.cs
+++ src/ObjectCloner/Internal/DeepCloneInternal.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 namespace ObjectCloner.Internal
@@ -12,9 +14,9 @@ namespace ObjectCloner.Internal
         ///     First argument is the object to be cloned, second is a map in which already-cloned objects are stored. Call with an empty Dictionary initially.
         /// </summary>
         /// <remarks>
-        ///     The <see cref="IDictionary{TKey,TValue}"/> argument is used for handling circular references and multiple references to the same object.
+        ///     The <see cref="Dictionary{TKey,TValue}"/> argument is used for handling circular references and multiple references to the same object.
         /// </remarks>
-        public static Func<T, IDictionary<object, object>, T> DeepCloner { get; }
+        public static Func<T, Dictionary<object, object>, T> DeepCloner { get; }
 
         static DeepCloneInternal()
         {
@@ -27,12 +29,12 @@ namespace ObjectCloner.Internal
             DeepCloner = CreateDeepCloneMethod();
         }
 
-        private static Func<T,IDictionary<object,object>,T> CreateDeepCloneMethod()
+        private static Func<T,Dictionary<object,object>,T> CreateDeepCloneMethod()
         {
             // Here is the method we need to create:
             // Lines prefixed by # only apply if T is a reference type.
             //
-            // (T original, IDictionary<object, object> dict) => {
+            // (T original, Dictionary<object, object> dict) => {
             //     if(original == null)
             //         return null;
             //
@@ -40,45 +42,115 @@ namespace ObjectCloner.Internal
             //#        return existingClone;
             //
             //     var clone = (T)input.MemberwiseClone();
-            //     dict.Add(original, clone);
+            //#     dict.Add(original, clone);
             //     
             //     // Code to recursively clone fields that are not deeply readonly (primitives, strings, etc. should not be copied)
             //
             //     return clone;
             // }
-            
-            
-            ParameterExpression originalParameter = Expression.Parameter(typeof(T));
-            ParameterExpression dictionaryParameter = Expression.Parameter(typeof(IDictionary<object,object>));
-            LabelTarget returnTarget = Expression.Label(typeof(T));
 
-            
-            Expression returnIfNullExpression = CreateReturnIfNullExpression(originalParameter, returnTarget);
 
-            var functionBlock = Expression.Block(
-                returnIfNullExpression,
-                Expression.Label(returnTarget, Expression.Constant(null, typeof(T)))
-            );
+            Type typeOfT = typeof(T);
+            ParameterExpression originalParameter = Expression.Parameter(typeOfT);
+            ParameterExpression dictionaryParameter = Expression.Parameter(typeof(Dictionary<object,object>));
+            ParameterExpression cloneVariable = Expression.Variable(typeOfT);
+            LabelTarget returnTarget = Expression.Label(typeOfT);
+
+            List<Expression> expressions = new List<Expression>(10);
             
-            var functionExpression = Expression.Lambda<Func<T, IDictionary<object, object>, T>>(functionBlock, originalParameter, dictionaryParameter);
+            expressions.Add(CreateReturnIfNullExpression(originalParameter, returnTarget));
+
+            if (!typeOfT.IsValueType)
+            {
+                expressions.Add(CreateReturnIfInDictionaryExpression(originalParameter, dictionaryParameter, returnTarget));
+            }
+            
+            
+            expressions.Add(CreateMemberwiseCloneExpression(originalParameter, cloneVariable));
+            
+            if (!typeOfT.IsValueType)
+            {
+                expressions.Add(CreateAddToDictionaryExpression(originalParameter, dictionaryParameter, cloneVariable));
+            }
+            
+            expressions.Add(Expression.Label(returnTarget, cloneVariable));
+
+
+            var functionBlock = Expression.Block(new[] {cloneVariable}, expressions);
+            
+            var functionExpression = Expression.Lambda<Func<T, Dictionary<object, object>, T>>(functionBlock, originalParameter, dictionaryParameter);
             return functionExpression.Compile();
         }
 
 
+
         private static ConditionalExpression CreateReturnIfNullExpression(ParameterExpression originalParameter, LabelTarget returnTarget)
         {
-            return Expression.IfThenElse(
+            // if(original == null)
+            //         return null;
+            
+            return Expression.IfThen(
                 Expression.ReferenceEqual(originalParameter, Expression.Constant(null)),
-                Expression.Constant(null),
                 Expression.Return(returnTarget, Expression.Constant(null, typeof(T)))
             );
         }
-        private static Expression CreateReturnIfInDictionaryExpression(ParameterExpression originalParameter, ParameterExpression dictionaryParameter)
+        private static Expression CreateReturnIfInDictionaryExpression(ParameterExpression originalParameter, ParameterExpression dictionaryParameter, LabelTarget returnTarget)
         {
-            return Expression.Throw(Expression.New(typeof(NotImplementedException)));
+            //    if(dict.TryGetValue(original, out object existingClone))
+            //        return existingClone;
+            
+            var tryGetValueMethod = typeof(Dictionary<object, object>).GetMethod("TryGetValue", BindingFlags.Instance | BindingFlags.Public);
+            Debug.Assert(tryGetValueMethod != null);
+            
+            var outTVariable = Expression.Variable(typeof(T));
+            return Expression.Block(
+                new[] { outTVariable },
+                Expression.IfThen(
+                        Expression.IsTrue(
+                                Expression.Call(
+                                    dictionaryParameter,
+                                    tryGetValueMethod,
+                                    originalParameter,
+                                    outTVariable
+                                    )),
+                        Expression.Return(returnTarget, outTVariable)
+                    )
+            );
+        }
+        
+        private static Expression CreateMemberwiseCloneExpression(ParameterExpression originalParameter, ParameterExpression cloneVariable)
+        {
+            MethodInfo cloneMethod = typeof(T).GetMethod("MemberwiseClone", BindingFlags.Instance | BindingFlags.NonPublic);
+            Debug.Assert(cloneMethod != null);
+            
+            var cloneExpression = Expression.Assign(
+                cloneVariable,
+                Expression.Convert(
+                    Expression.Call(
+                        originalParameter,
+                        cloneMethod
+                    ), 
+                    typeof(T)
+                ));
+
+            return cloneExpression;
+        }
+        
+        
+        private static Expression CreateAddToDictionaryExpression(ParameterExpression originalParameter, ParameterExpression dictionaryParameter, ParameterExpression cloneVariable)
+        {
+            
+            MethodInfo addMethod = typeof(Dictionary<object,object>).GetMethod("Add", BindingFlags.Instance | BindingFlags.Public);
+            Debug.Assert(addMethod != null);
+            return Expression.Call(
+                dictionaryParameter,
+                addMethod,
+                originalParameter,
+                cloneVariable
+            );
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static T Identity(T input, IDictionary<object, object> _) => input;
+        private static T Identity(T input, Dictionary<object, object> _) => input;
     }
 }
