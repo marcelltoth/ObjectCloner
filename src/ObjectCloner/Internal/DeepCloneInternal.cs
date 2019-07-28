ec0d11bb20558ba0b2b7a027a66499f46cef166a
Marcell Toth <marcell@marcelltoth.net>
2019-07-28 23:34:59 +0200

    DeepClone WIP
    Handles nulls.

--- src/ObjectCloner/Internal/DeepCloneInternal.cs
+++ src/ObjectCloner/Internal/DeepCloneInternal.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 
 namespace ObjectCloner.Internal
@@ -22,6 +23,59 @@ namespace ObjectCloner.Internal
                 DeepCloner = Identity;
                 return;
             }
+
+            DeepCloner = CreateDeepCloneMethod();
+        }
+
+        private static Func<T,IDictionary<object,object>,T> CreateDeepCloneMethod()
+        {
+            // Here is the method we need to create:
+            // Lines prefixed by # only apply if T is a reference type.
+            //
+            // (T original, IDictionary<object, object> dict) => {
+            //     if(original == null)
+            //         return null;
+            //
+            //#    if(dict.TryGetValue(original, out object existingClone))
+            //#        return existingClone;
+            //
+            //     var clone = (T)input.MemberwiseClone();
+            //     dict.Add(original, clone);
+            //     
+            //     // Code to recursively clone fields that are not deeply readonly (primitives, strings, etc. should not be copied)
+            //
+            //     return clone;
+            // }
+            
+            
+            ParameterExpression originalParameter = Expression.Parameter(typeof(T));
+            ParameterExpression dictionaryParameter = Expression.Parameter(typeof(IDictionary<object,object>));
+            LabelTarget returnTarget = Expression.Label(typeof(T));
+
+            
+            Expression returnIfNullExpression = CreateReturnIfNullExpression(originalParameter, returnTarget);
+
+            var functionBlock = Expression.Block(
+                returnIfNullExpression,
+                Expression.Label(returnTarget, Expression.Constant(null, typeof(T)))
+            );
+            
+            var functionExpression = Expression.Lambda<Func<T, IDictionary<object, object>, T>>(functionBlock, originalParameter, dictionaryParameter);
+            return functionExpression.Compile();
+        }
+
+
+        private static ConditionalExpression CreateReturnIfNullExpression(ParameterExpression originalParameter, LabelTarget returnTarget)
+        {
+            return Expression.IfThenElse(
+                Expression.ReferenceEqual(originalParameter, Expression.Constant(null)),
+                Expression.Constant(null),
+                Expression.Return(returnTarget, Expression.Constant(null, typeof(T)))
+            );
+        }
+        private static Expression CreateReturnIfInDictionaryExpression(ParameterExpression originalParameter, ParameterExpression dictionaryParameter)
+        {
+            return Expression.Throw(Expression.New(typeof(NotImplementedException)));
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
