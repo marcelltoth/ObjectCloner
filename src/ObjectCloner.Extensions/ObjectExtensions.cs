2078facde58c15e334a7068481681a02e1739a75
Marcell Toth <marcell@marcelltoth.net>
2019-07-30 22:15:12 +0200

    Create Extensions project

---
+++ src/ObjectCloner.Extensions/ObjectExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ObjectCloner.Extensions
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        ///     <para>Performs a deep clone on <paramref name="original" />.</para>
+        ///     <seealso cref="ObjectCloner.DeepClone{T}"/>
+        /// </summary>
+        /// <remarks>
+        ///     Equivalent with <code>ObjectCloner.DeepClone(original)</code>.
+        /// </remarks>
+        /// <param name="original">The object to clone.</param>
+        public static T DeepClone<T>(this T original) => ObjectCloner.DeepClone(original);
+        
+        /// <summary>
+        ///     <para>Performs a shallow clone on <paramref name="original" />.</para>
+        ///     <seealso cref="ObjectCloner.ShallowClone{T}"/>
+        /// </summary>
+        /// <remarks>
+        ///     Equivalent with <code>ObjectCloner.ShallowClone(original)</code>.
+        /// </remarks>
+        /// <param name="original">The object to clone.</param>
+        public static T ShallowClone<T>(this T original) => ObjectCloner.ShallowClone(original);
+    }
+}
