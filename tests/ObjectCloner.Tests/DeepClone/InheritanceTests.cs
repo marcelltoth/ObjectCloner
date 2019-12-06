766782a43078fe96c4c7d60040e53f26d4c1bb07
Marcell Toth <marcell@marcelltoth.net>
2019-12-06 12:50:07 +0100

    Build test case to reproduce issue #2

---
+++ tests/ObjectCloner.Tests/DeepClone/InheritanceTests.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+
+using Xunit;
+
+namespace ObjectCloner.Tests.DeepClone
+{
+    public class InheritanceTests
+    {
+        [Fact]
+        public void ClonesPropertyOnBaseClass()
+        {
+            var input = new MainTestClass
+            {
+                ListClassesBase = new List<SecondClass>()
+            };
+
+            var clone = ObjectCloner.DeepClone(input);
+            
+            Assert.NotEqual(input.ListClassesBase, clone.ListClassesBase);
+        }
+        
+        private class SecondClass
+        {
+            public int Value { get; set; }
+        }
+
+        private class BaseClass
+        {
+            public IList<SecondClass> ListClassesBase { get; set; }
+        }
+         
+        private class MainTestClass : BaseClass
+        {
+            public IList<SecondClass> ListClasses { get; set; }
+        }
+    }
+}
